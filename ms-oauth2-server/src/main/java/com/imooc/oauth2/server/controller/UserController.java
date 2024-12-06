cba689c9d1db585d2be73e77598f019018df2822
成立尧 <kariseven323@gmail.com>
2024-12-06 21:06:27 +0800

    增加安全退出操作

--- ms-oauth2-server/src/main/java/com/imooc/oauth2/server/controller/UserController.java
+++ ms-oauth2-server/src/main/java/com/imooc/oauth2/server/controller/UserController.java
@@ -4,8 +4,11 @@ import com.imooc.commons.model.domain.ResultInfo;
 import com.imooc.commons.model.domain.SignInIdentity;
 import com.imooc.commons.model.vo.SignInDinerInfo;
 import com.imooc.commons.utils.ResultInfoUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +37,37 @@ public class UserController {
         BeanUtils.copyProperties(signInIdentity, dinerInfo);
         return ResultInfoUtil.buildSuccess(request.getServletPath(), dinerInfo);
     }
+
+    /**
+     * 安全退出
+     *
+     * @param access_token
+     * @param authorization
+     * @return
+     */
+    @GetMapping("user/logout")
+    public ResultInfo logout(String access_token, String authorization) {
+        // 判断 access_token 是否为空，为空将 authorization 赋值给 access_token
+        if (StringUtils.isBlank(access_token)) {
+            access_token = authorization;
+        }
+        // 判断 authorization 是否为空
+        if (StringUtils.isBlank(access_token)) {
+            return ResultInfoUtil.buildSuccess(request.getServletPath(), "退出成功");
+        }
+        // 判断 bearer token 是否为空
+        if (access_token.toLowerCase().contains("bearer ".toLowerCase())) {
+            access_token = access_token.toLowerCase().replace("bearer ", "");
+        }
+        // 清除 redis token 信息
+        OAuth2AccessToken oAuth2AccessToken = redisTokenStore.readAccessToken(access_token);
+        if (oAuth2AccessToken != null) {
+            redisTokenStore.removeAccessToken(oAuth2AccessToken);
+            OAuth2RefreshToken refreshToken = oAuth2AccessToken.getRefreshToken();
+            redisTokenStore.removeRefreshToken(refreshToken);
+        }
+        return ResultInfoUtil.buildSuccess(request.getServletPath(), "退出成功");
+    }
+
 }
 
